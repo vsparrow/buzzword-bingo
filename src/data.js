cb3bf655a011e8032ece59a26bc73202a385a35b
johnymontana <lyonwj@gmail.com>
2018-09-22 07:09:10 -0400

    update buzzwords

--- src/data.js
+++ src/data.js
@@ -3,32 +3,42 @@ export const data = [
     title: "neo4j-graphql-js",
     description:
       "A GraphQL to Cypher execution layer for JavaScript GraphQL implementations.",
-    links: [{ text: "Some resource", url: "https://www.npmjs.com/package/neo4j-graphql-js" }],
+    links: [
+      {
+        text: "Some resource",
+        url: "https://www.npmjs.com/package/neo4j-graphql-js"
+      }
+    ],
     tags: ["JavaScript", "GraphQL", "node.js"]
   },
   {
     title: "Neo4j Sandbox",
-    description:"The Neo4j Sandbox enables you to get started with Neo4j, with built-in guides and sample datasets for popular use cases.",
+    description:
+      "The Neo4j Sandbox enables you to get started with Neo4j, with built-in guides and sample datasets for popular use cases.",
     links: [{ text: "Launch Sandbox", url: "https://neo4j.com/sandbox-v2/" }],
     tags: ["Hosted", "Datasets"]
   },
   {
     title: "GRANDstack",
-    description: "Fullstack application development with GraphQL, React, Apollo, and Neo4j Database",
+    description:
+      "Fullstack application development with GraphQL, React, Apollo, and Neo4j Database",
     links: [
-        { text: "GRANDstack.io", 
-           url: "https://grandstack.io/" 
-        }
+      {
+        text: "GRANDstack.io",
+        url: "https://grandstack.io/"
+      }
     ],
     tags: ["Fullstack", "GraphQL"]
   },
   {
     title: "neovis.js",
-    description: "Neo4j + vis.js = neovis.js. Graph visualizations in the browser with data from Neo4j.",
+    description:
+      "Neo4j + vis.js = neovis.js. Graph visualizations in the browser with data from Neo4j.",
     links: [
-        { text: "Github", 
-           url: "https://github.com/neo4j-contrib/neovis.js/" 
-        }
+      {
+        text: "Github",
+        url: "https://github.com/neo4j-contrib/neovis.js/"
+      }
     ],
     tags: ["Visualization", "JavaScript"]
   },
@@ -36,9 +46,10 @@ export const data = [
     title: "Neo4j ETL Tool",
     description: "Automated ETL from relational to graph.",
     links: [
-        { text: "Getting started", 
-           url: "https://neo4j.com/developer/neo4j-etl/" 
-        }
+      {
+        text: "Getting started",
+        url: "https://neo4j.com/developer/neo4j-etl/"
+      }
     ],
     tags: ["RDBMS", "Data import"]
   },
@@ -46,9 +57,10 @@ export const data = [
     title: ".NET Neo4j Driver",
     description: "The official Neo4j driver for .NET",
     links: [
-        { text: "Get started", 
-           url: "https://neo4j.com/developer/dotnet/" 
-        }
+      {
+        text: "Get started",
+        url: "https://neo4j.com/developer/dotnet/"
+      }
     ],
     tags: [".NET", "Neo4j Driver"]
   },
@@ -56,9 +68,10 @@ export const data = [
     title: "Java Neo4j Driver",
     description: "The official Neo4j driver for Java",
     links: [
-        { text: "Getting started guide", 
-           url: "https://neo4j.com/developer/java/" 
-        }
+      {
+        text: "Getting started guide",
+        url: "https://neo4j.com/developer/java/"
+      }
     ],
     tags: ["Java", "Neo4j Driver"]
   },
@@ -66,9 +79,10 @@ export const data = [
     title: "Neo4j JavaScript Driver",
     description: "The official Neo4j driver for JavaScript",
     links: [
-        { text: "Get started", 
-           url: "https://neo4j.com/developer/javascript/" 
-        }
+      {
+        text: "Get started",
+        url: "https://neo4j.com/developer/javascript/"
+      }
     ],
     tags: ["JavaScript", "Neo4j Driver"]
   },
@@ -76,9 +90,10 @@ export const data = [
     title: "Python Neo4j Driver",
     description: "The official Neo4j driver for Python",
     links: [
-        { text: "Get started", 
-           url: "https://neo4j.com/developer/python/" 
-        }
+      {
+        text: "Get started",
+        url: "https://neo4j.com/developer/python/"
+      }
     ],
     tags: ["Python", "Neo4j Driver"]
   },
@@ -86,9 +101,10 @@ export const data = [
     title: "Go Neo4j Driver",
     description: "The official Neo4j driver for Go",
     links: [
-        { text: "Get started", 
-           url: "https://github.com/neo4j/neo4j-go-driver" 
-        }
+      {
+        text: "Get started",
+        url: "https://github.com/neo4j/neo4j-go-driver"
+      }
     ],
     tags: ["Go", "Neo4j Driver"]
   },
@@ -96,9 +112,10 @@ export const data = [
     title: "seabolt",
     description: "Neo4j Bolt Connector for C",
     links: [
-        { text: "Github", 
-           url: "https://github.com/neo4j-drivers/seabolt" 
-        }
+      {
+        text: "Github",
+        url: "https://github.com/neo4j-drivers/seabolt"
+      }
     ],
     tags: ["C", "Neo4j Driver"]
   },
@@ -106,13 +123,14 @@ export const data = [
     title: "Neo4j PHP Client",
     description: "Using Neo4j With PHP",
     links: [
-        { text: "Get started", 
-           url: "https://neo4j.com/developer/php/" 
-        },
-        {
-            text: "Github",
-            url: "https://github.com/neo4j-drivers/seabolt"
-        }
+      {
+        text: "Get started",
+        url: "https://neo4j.com/developer/php/"
+      },
+      {
+        text: "Github",
+        url: "https://github.com/neo4j-drivers/seabolt"
+      }
     ],
     tags: ["PHP", "Neo4j Driver"]
   },
@@ -120,30 +138,324 @@ export const data = [
     title: "Ruby",
     description: "Using Neo4j With Ruby",
     links: [
-        { text: "Get started", 
-           url: "https://neo4j.com/developer/ruby/" 
-        }
+      {
+        text: "Get started",
+        url: "https://neo4j.com/developer/ruby/"
+      }
     ],
     tags: ["Ruby", "Neo4j Driver"]
   },
   {
     title: "Pyneo",
-    description: "Py2neo is a client library and toolkit for working with Neo4j from within Python applications and from the command line.",
+    description:
+      "Py2neo is a client library and toolkit for working with Neo4j from within Python applications and from the command line.",
     links: [
-        { text: "Documentation", 
-           url: "https://py2neo.org/v4/" 
-        }
+      {
+        text: "Documentation",
+        url: "https://py2neo.org/v4/"
+      }
     ],
-    tags: [".NET", "Neo4j Driver"]
+    tags: ["Python", "Neo4j Driver"]
   },
   {
-    title: "",
-    description: "",
+    title: "Spring Data Neo4j",
+    description:
+      "This project provides Spring Data support for the Neo4j Graph Database, including annotated POJOs, SD-Repositories and Neo4j-Template.",
     links: [
-        { text: "", 
-           url: "" 
-        }
+      {
+        text: "Guide",
+        url: "https://neo4j.com/developer/spring-data-neo4j/"
+      }
     ],
-    tags: [""]
+    tags: ["Java"]
+  },
+  {
+    title: "Neode",
+    description: "Neo4j OGM for Node JS",
+    links: [
+      {
+        text: "Github",
+        url: "https://github.com/adam-cowley/neode"
+      }
+    ],
+    tags: ["JavaScript"]
+  },
+  {
+    title: "Graph Apps",
+    description: "Graph applications built on the Neo4j Graph Platform",
+    links: [
+      {
+        text: "Github",
+        url: "https://github.com/neo4j-apps/"
+      }
+    ],
+    tags: ["JavaScript", "Neo4j Desktop"]
+  },
+  {
+    title: "ml-models",
+    description: "Machine Learning Procedures and Functions for Neo4j",
+    links: [
+      {
+        text: "Github",
+        url: "https://github.com/neo4j-graph-analytics/ml-models"
+      },
+      {
+        text: "Blog post",
+        url:
+          "https://towardsdatascience.com/graphs-and-linear-regression-734d1446e9cd"
+      }
+    ],
+    tags: ["Machine Learning"]
+  },
+  {
+    title: "Spatial",
+    description: "Geospatial functionality in Neo4j",
+    links: [
+      {
+        text: "Docs",
+        url:
+          "https://neo4j.com/docs/developer-manual/current/cypher/functions/spatial/"
+      },
+      {
+        text: "SpaceTime Reviews Example",
+        url:
+          "https://medium.com/neo4j/working-with-neo4j-date-and-spatial-types-in-a-react-js-app-5475b5042b50"
+      },
+      {
+        text: "Spatial features blog post",
+        url:
+          "https://medium.com/neo4j/whats-new-in-neo4j-spatial-features-586d69cda8d0"
+      }
+    ],
+    tags: ["Geospatial"]
+  },
+  {
+    title: "APOC",
+    description:
+      "The APOC library consists of many (about 450) procedures and functions to help with many different tasks in areas like data integration, graph algorithms or data conversion.",
+    links: [
+      {
+        text: "Docs",
+        url:
+          "The APOC library consists of many (about 450) procedures and functions to help with many different tasks in areas like data integration, graph algorithms or data conversion."
+      }
+    ],
+    tags: ["Plugin"]
+  },
+  {
+    title: "Neo4j Graph Algorithms",
+    description: "Efficient Graph Algorithms for Neo4j",
+    links: [
+      {
+        text: "Docs",
+        url: "https://neo4j.com/docs/graph-algorithms/current/"
+      }
+    ],
+    tags: ["Algorithms"]
+  },
+  {
+    title: "OpenStreetMap",
+    description: "OSM Data Model for Neo4j",
+    links: [
+      {
+        text: "Github",
+        url: "https://github.com/neo4j-contrib/osm"
+      }
+    ],
+    tags: ["Geospatial"]
+  },
+  {
+    title: "Graph Gist",
+    description:
+      "GraphGists are teaching tools which allow you to explore how data in a particular domain would be modeled as a graph.",
+    links: [
+      {
+        text: "Graph Gist Portal",
+        url: "https://portal.graphgist.org/",
+        text: "Example Graph Gists",
+        url: "https://neo4j.com/graphgists/"
+      }
+    ],
+    tags: ["Gist"]
+  },
+  {
+    title: "Tableau",
+    description: "Neo4j Tableau Integration via WDC",
+    links: [
+      {
+        text: "Github",
+        url: "https://github.com/neo4j-contrib/neo4j-tableau"
+      }
+    ],
+    tags: ["Visualization"]
+  },
+  {
+    title: "Kafka",
+    description: "Neo4j Kafka integrations",
+    links: [
+      {
+        text: "Github",
+        url: "https://github.com/neo4j-contrib/neo4j-kafka"
+      }
+    ],
+    tags: ["Import", "Streaming"]
+  },
+  {
+    title: "Linode",
+    description: "Cloud hosting for developers. Use code 'GraphHack2018' for free access",
+    links: [
+      {
+        text: "Getting started",
+        url: "https://www.linode.com/docs/getting-started/"
+      },
+      {
+          text: "Docs",
+          url: "https://linode.com/docs/"
+      }
+    ],
+    tags: ["Hosting"]
+  },
+  {
+    title: "Graphistry",
+    description: "GPU Visual Analytics",
+    links: [
+      {
+        text: "Homepage",
+        url: "https://www.graphistry.com/"
+      },
+      {
+          text: "Twitter Trolls Example",
+          url: "https://github.com/graphistry/pygraphistry/blob/master/demos/Neo4jTwitter.ipynb"
+      }
+    ],
+    tags: ["Visualization"]
+  },
+  {
+    title: "NLP With GraphAware",
+    description: "Natural language processing capabilities in Neo4j ",
+    links: [
+      {
+        text: "Github",
+        url: "https://github.com/graphaware/neo4j-nlp"
+      },
+      {
+          text: "Blog posts / examples",
+          url: "https://graphaware.com/blog/nlp/"
+      },
+      {
+          text: "Twitter example",
+          url: "https://medium.com/neo4j/using-nlp-in-neo4j-ac40bc92196f"
+      }
+    ],
+    tags: ["Natural Language Processing"]
+  },
+  {
+    title: "yFiles",
+    description: "Add high-quality diagramming functionality to your own software applications.",
+    links: [
+      {
+          text: "Tutorial",
+          url: "https://www.yworks.com/blog/neo4j-visualization-like-a-pro"
+      },
+      {
+        text: "Example (Github)",
+        url: "https://www.yworks.com/blog/neo4j-visualization-like-a-pro"
+      },
+      {
+        text: "Live Demo",
+        url: "https://live.yworks.com/demos/toolkit/neo4j/index.html"
+      },
+      {
+        text: "Download",
+        url: "https://1drv.ms/u/s!AtNJ4Wu6PijfgdsIjo7vwTWq-9Ojpw"
+      }
+    ],
+    tags: ["Visualization"]
+  },
+  {
+    title: "Kubernetes",
+    description: "Automated container deployment, scaling, and management",
+    links: [
+      {
+        text: "Github",
+        url: "https://github.com/neo4j-contrib/kubernetes-neo4j"
+      },
+      {
+          text: "Blog post",
+          url: "https://medium.com/google-cloud/launching-neo4j-on-googles-kubernetes-marketplace-97c23c94e960"
+      }
+    ],
+    tags: ["Deployment", "Scaling"]
+  },
+  {
+    title: "neo4j-elasticsearch",
+    description: "Neo4j integration for ElasticSearch",
+    links: [
+      {
+        text: "Github",
+        url: "https://github.com/neo4j-contrib/neo4j-elasticsearch"
+      }
+    ],
+    tags: ["Search"]
+  },
+  {
+    title: "Graph Commons",
+    description: "Graph Commons is a collaborative platform for making, analyzing and publishing network maps.",
+    links: [
+      {
+        text: "Homepage",
+        url: "https://graphcommons.com/"
+      }
+    ],
+    tags: ["Import", "Visualization"]
+  },
+  {
+    title: "Cloud",
+    description: "There are many options for Cloud deployment of Neo4j.",
+    links: [
+      {
+        text: "AWS Tutorial",
+        url: "https://medium.com/neo4j/neo4j-community-on-aws-quick-start-65736644ebc"
+      },
+      {
+          text: "Cloud guide",
+          url: "https://neo4j.com/developer/guide-cloud-deployment/"
+      }
+
+    ],
+    tags: ["Cloud"]
+  },
+  {
+    title: "Yelp public Dataset",
+    description: "Businesses, users, reviews, etc from Yelp, loaded into Neo4j. ",
+    links: [
+      {
+        text: "Access here with user:password of reviews:letmein",
+        url: "https://47b53882.databases.neo4j.io/browser/"
+      }
+    ],
+    tags: ["Dataset"]
+  },
+  {
+    title: "StackOverflow Dataset",
+    description: "StackOverflow imported into Neo4j.",
+    links: [
+      {
+        text: "Access heere with user:password of stackoverflow:stackoverflow",
+        url: "https://stackdata.neo4jlabs.com:7473/browser/"
+      }
+    ],
+    tags: ["Dataset"]
+  },
+  {
+    title: "World Cup Dataset",
+    description: "Every world cup match imported into Neo4j and exposed via GraphQL.",
+    links: [
+      {
+        text: "Blog post",
+        url: "https://medium.freecodecamp.org/building-the-2018-world-cup-graphql-api-fab40ccecb9e"
+      }
+    ],
+    tags: ["Dataset", "GraphQL"]
   }
 ];
